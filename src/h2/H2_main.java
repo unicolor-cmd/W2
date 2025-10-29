3c0f31b65177faf618ce210f70a220af535002f1
regular <regular@regular.local>
2025-10-29 13:09:38 +0100

    h2

---
+++ src/h2/H2_main.java
@@ -0,0 +1,50 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+        int k = 2;
+        int min = 0;
+        int max = 0;
+
+        if (i < j) {
+            if (i < k) {
+                min = i;
+            }
+            if (i > k) {
+                min = k;
+            }
+
+            if (j < k) {
+                max = k;
+            }
+            if (j > k) {
+                max = j;
+            }
+        }
+        if (i > j) {
+            if (i > k) {
+                max = i;
+            }
+            if (i < k) {
+                max = k;
+            }
+
+            if (j < k) {
+                min = j;
+            }
+            if (j > k) {
+                min = k;
+            }
+        }
+
+        System.out.println("min:");
+        System.out.println(min);
+        System.out.println();
+
+        System.out.println("max:");
+        System.out.println(max);
+        System.out.println();
+    }
+}
--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -3,6 +3,7 @@ package p1;
 import java.util.Scanner;
 
 public class P1_main {
+    // 'psvm' adds public static void main(String[] args) {
     // cmd shift f in eclipse?
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
