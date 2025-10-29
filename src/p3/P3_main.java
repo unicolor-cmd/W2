9b87c6992668d3c4ed5cb3522a80ea0c7136ce51
regular <regular@regular.local>
2025-10-29 12:48:10 +0100

    p3

---
+++ src/p3/P3_main.java
@@ -0,0 +1,18 @@
+package p3;
+
+public class P3_main {
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 1;
+        int k = 2;
+        if (i < j) {
+            System.out.println("A");
+        }
+        if (i < k) {
+            System.out.println("B");
+        }
+        if (j < k) {
+            System.out.println("C");
+        }
+    }
+}
